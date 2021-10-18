dd3f62fdb6f00305b2dd2efaf9c00f37fe4ada43
Florian Dayre <Florian@macbook-air-de-florian.home>
2021-10-18 21:46:47 +0200

    modif

---
+++ Test_Projet2D/New Unity Project/Assets/Scripts/CheckPoint.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{
+    private Transform playerSpawn;
+
+    void Awake()
+    {
+        playerSpawn = GameObject.FindGameObjectWithTag("PlayerSpawn").transform;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            playerSpawn.position = transform.position;
+            gameObject.GetComponent<BoxCollider2D>().enabled = false;
+        }
+    }
+}
--- Test_Projet2D/New Unity Project/Assets/Scripts/PlayerMovement.cs
+++ Test_Projet2D/New Unity Project/Assets/Scripts/PlayerMovement.cs
@@ -5,8 +5,9 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed;
     public float jumpForce;
 
-    public bool isJumping;
-    public bool isGrounded;
+    private bool isJumping;
+    private bool isGrounded;
+    public bool isClimbing;
 
     public Transform groundCheck;
     public float groundCheckRadius;
@@ -16,21 +17,23 @@ public class PlayerMovement : MonoBehaviour
     public Animator animator;
     public SpriteRenderer spriteRenderer;
 
-    private Vector3 velocity = Vector3.zero;
+    public Vector3 velocity = Vector3.zero;
     private float horizontalMovement;
+    private float verticalMovement;
 
     void Update()
     {
         horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
-        
-        if(Input.GetButtonDown("Jump") && isGrounded == true)
+        verticalMovement = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
         {
             //rb.gravityScale = 0.0f; Pour changer la gravité
             isJumping = true;
         }
 
         //Animation de saut
-        if(isGrounded == true)
+        if(isGrounded)
         {
             animator.SetBool("IsJumping", false);
         }
@@ -45,23 +48,34 @@ public class PlayerMovement : MonoBehaviour
 
         //Animation Course ou Attente
         animator.SetFloat("Speed", characterVelocity);
+        animator.SetBool("isClimbing", isClimbing);
     }
 
     void FixedUpdate()
     {
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, collisionLayers);
-        MovePlayer(horizontalMovement);
+        MovePlayer(horizontalMovement, verticalMovement);
     }
 
-    void MovePlayer(float _horizontalMovement)
+    void MovePlayer(float _horizontalMovement, float _verticalMovement)
     {
-        Vector3 targetVelocity = new Vector2(_horizontalMovement, rb.velocity.y);
-        rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, .05f);
-    
-        if(isJumping == true)
+        if(!isClimbing)
         {
-            rb.AddForce(new Vector2(0f, jumpForce));
-            isJumping = false;
+            //Déplacement horizontal du joueur
+            Vector3 targetVelocity = new Vector2(_horizontalMovement, rb.velocity.y);
+            rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, .05f);
+        
+            if(isJumping)
+            {
+                rb.AddForce(new Vector2(0f, jumpForce));
+                isJumping = false;
+            }
+        }
+        else
+        {
+            //Déplacement vertical du joueur
+            Vector3 targetVelocity = new Vector2(0, _verticalMovement);
+            rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, .05f);
         }
     }
 
